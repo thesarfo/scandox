700cc7614f6cee14a9aa9c8a400ea13f7175e2d5
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-02-23 23:12:14 +0000

    get top down perspective of doc/image

--- scan.py
+++ scan.py
@@ -1,4 +1,4 @@
-# from python_imagesearch.transform import four_point_transform
+from imutils.perspective import four_point_transform
 from skimage.filters import threshold_local
 import numpy as np
 import argparse
@@ -42,3 +42,13 @@ cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
 cv2.imshow("Outline", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+T = threshold_local(warped, 11, offset = 10, method = "gaussian")
+warped = (warped > T).astype("uint8") * 255
+
+print("STEP 3: Apply perspective transform")
+cv2.imshow("Original", imutils.resize(orig, height = 650))
+cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+cv2.waitKey(0)
