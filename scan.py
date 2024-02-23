268107725f46ec18a467fa5d3e2a28cfca6afb09
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-02-23 23:25:16 +0000

    rename output

--- scan.py
+++ scan.py
@@ -18,7 +18,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(gray, 75, 200)
 
-print("STEP 1: Edge Detection")
+print("Edge Detection")
 cv2.imshow("Image", image)
 cv2.imshow("Edged", edged)
 cv2.waitKey(0)
@@ -37,7 +37,7 @@ for c in cnts:
         screenCnt = approx
         break
 
-print("STEP 2: Find contours of paper")
+print("Finding contours of paper")
 cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
 cv2.imshow("Outline", image)
 cv2.waitKey(0)
@@ -48,7 +48,7 @@ warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
 T = threshold_local(warped, 11, offset = 10, method = "gaussian")
 warped = (warped > T).astype("uint8") * 255
 
-print("STEP 3: Apply perspective transform")
+print("Applying perspective transform")
 cv2.imshow("Original", imutils.resize(orig, height = 650))
 cv2.imshow("Scanned", imutils.resize(warped, height = 650))
 cv2.waitKey(0)
